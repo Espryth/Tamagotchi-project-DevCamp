589ed69064a376f31c5d2cae61d93bb7f34834c0
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-12 13:05:39 -0600

    BathItem and ToyItem constructor

--- Core/src/main/java/com/maimai/tamagotchi/item/bath/BathItem.java
+++ Core/src/main/java/com/maimai/tamagotchi/item/bath/BathItem.java
@@ -1,4 +1,34 @@
 package com.maimai.tamagotchi.item.bath;
 
-public class BathItem {
+import com.maimai.tamagotchi.builder.IBuilder;
+import com.maimai.tamagotchi.item.*;
+
+public class BathItem extends AbstractItem {
+
+    public BathItem(DefaultType defaultType, ItemExecutor itemExecutor) {
+        super(defaultType, ItemType.BATH, itemExecutor);
+    }
+
+    public static class Builder implements IBuilder<Item> {
+
+        private ItemExecutor itemExecutor;
+        private DefaultType defaultType;
+
+        public BathItem.Builder createExecutor(ItemExecutor itemExecutor) {
+            this.itemExecutor = itemExecutor;
+            return this;
+        }
+
+        public BathItem.Builder setType(DefaultType type) {
+            this.defaultType = type;
+            return this;
+        }
+
+        @Override
+        public Item build() {
+            return new BathItem(defaultType, itemExecutor);
+        }
+    }
 }
+
+
--- Core/src/main/java/com/maimai/tamagotchi/item/toy/ToyItem.java
+++ Core/src/main/java/com/maimai/tamagotchi/item/toy/ToyItem.java
@@ -1,4 +1,34 @@
 package com.maimai.tamagotchi.item.toy;
 
-public class ToyItem {
+import com.maimai.tamagotchi.builder.IBuilder;
+import com.maimai.tamagotchi.item.*;
+
+public class ToyItem extends AbstractItem {
+
+    public ToyItem(DefaultType defaultType, ItemExecutor itemExecutor) {
+        super(defaultType, ItemType.TOY, itemExecutor);
+    }
+
+    public static class Builder implements IBuilder<Item> {
+
+        private ItemExecutor itemExecutor;
+        private DefaultType defaultType;
+
+        public ToyItem.Builder createExecutor(ItemExecutor itemExecutor) {
+            this.itemExecutor = itemExecutor;
+            return this;
+        }
+
+        public ToyItem.Builder setType(DefaultType type) {
+            this.defaultType = type;
+            return this;
+        }
+
+        @Override
+        public Item build() {
+            return new ToyItem(defaultType, itemExecutor);
+        }
+    }
 }
+
+
