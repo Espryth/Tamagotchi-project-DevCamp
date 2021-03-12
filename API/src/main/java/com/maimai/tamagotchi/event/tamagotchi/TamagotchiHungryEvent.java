5e1dd2bce76571c9701e521fcedddd6fa9bbd977
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-12 14:13:02 -0600

    Modified hungry event

--- API/src/main/java/com/maimai/tamagotchi/event/tamagotchi/TamagotchiHungryEvent.java
+++ API/src/main/java/com/maimai/tamagotchi/event/tamagotchi/TamagotchiHungryEvent.java
@@ -2,18 +2,14 @@ package com.maimai.tamagotchi.event.tamagotchi;
 
 import com.maimai.tamagotchi.tamagotchi.Tamagotchi;
 
-public class TamagotchiHungryEvent extends TamagotchiEvent{
+public class TamagotchiHungryEvent extends TamagotchiEvent {
 
     private boolean hungry;
 
-    public TamagotchiHungryEvent(Tamagotchi tamagotchi){
+    public TamagotchiHungryEvent(Tamagotchi tamagotchi) {
         super(tamagotchi);
-        if (tamagotchi.getHunger().getValue() == 0){
-            this.hungry = true;
-        }
     }
-
     public boolean isHungry(){
-        return hungry;
+        return getTamagotchi().getHunger().getValue() == 0;
     }
 }
