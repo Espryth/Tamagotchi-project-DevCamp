bb2edcb7514b10c317bba77600d38c60cec8618c
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-17 14:02:25 -0600

    TamagotchiStatsChangeEvent and TamagotchiStatsChangeListener updated again

--- API/src/main/java/com/maimai/tamagotchi/event/tamagotchi/TamagotchiStatsChangeEvent.java
+++ API/src/main/java/com/maimai/tamagotchi/event/tamagotchi/TamagotchiStatsChangeEvent.java
@@ -5,12 +5,8 @@ import com.maimai.tamagotchi.tamagotchi.Tamagotchi;
 
 public class TamagotchiStatsChangeEvent extends TamagotchiEvent {
 
-    private Statistic<Double> statistic;
-    public TamagotchiStatsChangeEvent(Tamagotchi tamagotchi, Statistic<Double> statistic) {
+    public TamagotchiStatsChangeEvent(Tamagotchi tamagotchi) {
         super(tamagotchi);
-        this.statistic = statistic;
-    }
-    public boolean statsIsZero(){
-        return statistic.getValue() == 0;
+
     }
 }
--- Core/src/main/java/com/maimai/tamagotchi/event/TamagotchiStatsChangeListener.java
+++ Core/src/main/java/com/maimai/tamagotchi/event/TamagotchiStatsChangeListener.java
@@ -2,16 +2,21 @@ package com.maimai.tamagotchi.event;
 
 import com.maimai.tamagotchi.event.listener.Listener;
 import com.maimai.tamagotchi.event.tamagotchi.TamagotchiStatsChangeEvent;
+import com.maimai.tamagotchi.tamagotchi.Tamagotchi;
 
 public class TamagotchiStatsChangeListener implements Listener {
     @EventHandler
-    public void onTamagotchiStatsChange(TamagotchiStatsChangeEvent event){
-        if (event.statsIsZero()){
-            event.getTamagotchi().getHealth().decrement(20D);
-        }else{
+    public void onTamagotchiStatsChange(TamagotchiStatsChangeEvent event) {
+
+        Tamagotchi tamagotchi = event.getTamagotchi();
+        if (tamagotchi.getHappiness().getValue() == 0 && tamagotchi.getHunger().getValue() == 0 &&
+                tamagotchi.getThirst().getValue() == 0){
+            event.getTamagotchi().getHealth().decrement(40D);
+        }else if (tamagotchi.getHappiness().getValue() == 0 && tamagotchi.getHunger().getValue() == 0 ||
+                tamagotchi.getHappiness().getValue() == 0 && tamagotchi.getThirst().getValue() == 0  ){
             event.getTamagotchi().getHealth().decrement(30D);
+        }else  if (tamagotchi.getThirst().getValue() == 0 || tamagotchi.getHunger().getValue() == 0) {
+            tamagotchi.getHealth().decrement(20D);
         }
     }
 }
-
-
