d95e6c1ec920c20ccec1480667add5c1f6051434
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-17 14:03:52 -0600

    CatTamagotchi actions modifications

--- Core/src/main/java/com/maimai/tamagotchi/tamagotchi/impl/CatTamagotchi.java
+++ Core/src/main/java/com/maimai/tamagotchi/tamagotchi/impl/CatTamagotchi.java
@@ -1,7 +1,9 @@
 package com.maimai.tamagotchi.tamagotchi.impl;
 
 import com.maimai.tamagotchi.ProgramCore;
+import com.maimai.tamagotchi.event.tamagotchi.TamagotchiStatsChangeEvent;
 import com.maimai.tamagotchi.tamagotchi.AbstractTamagotchi;
+import com.maimai.tamagotchi.tamagotchi.Tamagotchi;
 import com.maimai.tamagotchi.tamagotchi.TamagotchiType;
 import com.maimai.tamagotchi.tamagotchi.action.TamagotchiAction;
 
@@ -17,22 +19,24 @@ public class CatTamagotchi extends AbstractTamagotchi{
     public void registerActions() {
         registerAction("Play", new TamagotchiAction.Builder()
                 .createExecutor(player -> {
-                    player.getTamagotchi().getHappiness().increase(20D);
-                    player.getTamagotchi().getHunger().decrement(10D);
-                    player.getTamagotchi().getThirst().decrement(10D);
+                    core.getEventRegister().callEvent(new TamagotchiStatsChangeEvent(player.getTamagotchi()));
+                    player.getTamagotchi().getHappiness().increase(40D);
+                    player.getTamagotchi().getHunger().decrement(30D);
+                    player.getTamagotchi().getThirst().decrement(30D);
                     player.getTamagotchi().getDirty().increase(30D);
                 }).build());
 
         registerAction("Pet", new TamagotchiAction.Builder()
                 .createExecutor(player -> {
-                    player.getTamagotchi().getHappiness().decrement(20D);
-                    player.getTamagotchi().getDirty().decrement(40D);
+                    core.getEventRegister().callEvent(new TamagotchiStatsChangeEvent(player.getTamagotchi()));
+                    player.getTamagotchi().getHappiness().increase(20D);
                     System.out.println(player.getTamagotchi().getName()+" is purring");
                 }).build());
 
         registerAction("Bath", new TamagotchiAction.Builder()
                 .createExecutor(player -> {
-                    player.getTamagotchi().getHappiness().decrement(10D);
+                    core.getEventRegister().callEvent(new TamagotchiStatsChangeEvent(player.getTamagotchi()));
+                    player.getTamagotchi().getHappiness().decrement(20D);
                     System.out.println(player.getTamagotchi().getName()+" is very angry");
                 }).build());
     }
