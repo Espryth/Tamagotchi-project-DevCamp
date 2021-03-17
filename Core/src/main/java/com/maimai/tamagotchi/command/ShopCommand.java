b29e5fc2e4ae24fac5f0110a56c4340f357040c6
BryanGamingDV <davidbryanrs@gmail.com>
2021-03-17 10:00:57 -0500

    Fixed something..

--- Core/src/main/java/com/maimai/tamagotchi/command/ShopCommand.java
+++ Core/src/main/java/com/maimai/tamagotchi/command/ShopCommand.java
@@ -6,7 +6,6 @@ import com.maimai.tamagotchi.module.Module;
 public class ShopCommand implements CommandClass {
 
     @Command(name = "shop", usage = "/shop [buy/sell] [item]")
-    public void executeShopCommand(Module module) {
-        module.start();
+    public void executeShopCommand(String args) {
     }
 }
