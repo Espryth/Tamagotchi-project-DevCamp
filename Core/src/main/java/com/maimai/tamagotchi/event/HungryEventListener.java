13e195dcf5e0f8a5ebd07804753237ca33a1ffc4
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-17 11:02:35 -0600

    HungryEventListener and CatTamagotchi update

--- Core/src/main/java/com/maimai/tamagotchi/event/HungryEventListener.java
+++ Core/src/main/java/com/maimai/tamagotchi/event/HungryEventListener.java
@@ -1,13 +1,15 @@
 package com.maimai.tamagotchi.event;
 
-import com.maimai.tamagotchi.event.EventHandler;
 import com.maimai.tamagotchi.event.listener.Listener;
 import com.maimai.tamagotchi.event.tamagotchi.TamagotchiHungryEvent;
 
 public class HungryEventListener implements Listener {
     @EventHandler
     public void onHungryEvent(TamagotchiHungryEvent event){
+        if (event.isHungry()){
+            System.out.println("is hungry");
         }
     }
+}
 
 
--- Core/src/main/java/com/maimai/tamagotchi/tamagotchi/impl/CatTamagotchi.java
+++ Core/src/main/java/com/maimai/tamagotchi/tamagotchi/impl/CatTamagotchi.java
@@ -1,17 +1,21 @@
 package com.maimai.tamagotchi.tamagotchi.impl;
 
+import com.maimai.tamagotchi.ProgramCore;
 import com.maimai.tamagotchi.tamagotchi.AbstractTamagotchi;
 import com.maimai.tamagotchi.tamagotchi.TamagotchiType;
 import com.maimai.tamagotchi.tamagotchi.action.TamagotchiAction;
 
-public class CatTamagotchi extends AbstractTamagotchi {
-    public CatTamagotchi(String id, String name) {
+
+public class CatTamagotchi extends AbstractTamagotchi{
+    private final ProgramCore core;
+    public CatTamagotchi(ProgramCore core, String id, String name) {
         super(id, name, TamagotchiType.CAT);
+        this.core = core;
     }
 
     @Override
     public void registerActions() {
-        registerAction("Jugar", new TamagotchiAction.Builder()
+        registerAction("Play", new TamagotchiAction.Builder()
                 .createExecutor(player -> {
                     player.getTamagotchi().getHappiness().increase(20D);
                     player.getTamagotchi().getHunger().decrement(10D);
@@ -19,17 +23,18 @@ public class CatTamagotchi extends AbstractTamagotchi {
                     player.getTamagotchi().getDirty().increase(30D);
                 }).build());
 
-        registerAction("Acariciar", new TamagotchiAction.Builder()
+        registerAction("Pet", new TamagotchiAction.Builder()
                 .createExecutor(player -> {
                     player.getTamagotchi().getHappiness().decrement(20D);
                     player.getTamagotchi().getDirty().decrement(40D);
                     System.out.println(player.getTamagotchi().getName()+" is purring");
                 }).build());
 
-        registerAction("Bañar", new TamagotchiAction.Builder()
+        registerAction("Bath", new TamagotchiAction.Builder()
                 .createExecutor(player -> {
                     player.getTamagotchi().getHappiness().decrement(10D);
                     System.out.println(player.getTamagotchi().getName()+" is very angry");
                 }).build());
     }
+
 }
