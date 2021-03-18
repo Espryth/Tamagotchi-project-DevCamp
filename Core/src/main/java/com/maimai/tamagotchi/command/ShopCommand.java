c672374e5eed15e4412e625dbba92590f8269b8c
KubiG25 <32436056+KubiG25@users.noreply.github.com>
2021-03-17 22:40:59 -0500

    Shop Command code optimization

--- Core/src/main/java/com/maimai/tamagotchi/command/ShopCommand.java
+++ Core/src/main/java/com/maimai/tamagotchi/command/ShopCommand.java
@@ -1,16 +1,13 @@
 package com.maimai.tamagotchi.command;
 
 import com.maimai.tamagotchi.ProgramCore;
-import com.maimai.tamagotchi.TamagotchiCore;
 import com.maimai.tamagotchi.command.annotation.Command;
 import com.maimai.tamagotchi.command.annotation.OptArg;
 import com.maimai.tamagotchi.inventory.Inventory;
 import com.maimai.tamagotchi.item.DefaultType;
-import com.maimai.tamagotchi.item.Item;
 import com.maimai.tamagotchi.item.ItemType;
 import com.maimai.tamagotchi.item.food.FoodType;
 import com.maimai.tamagotchi.item.toy.ToyType;
-import com.maimai.tamagotchi.module.Module;
 import com.maimai.tamagotchi.player.Player;
 import com.maimai.tamagotchi.shop.ShopMain;
 import com.maimai.tamagotchi.shop.action.ShopAction;
@@ -31,114 +28,94 @@ public class ShopCommand implements CommandClass {
         Player player = core.getPlayer();
         Inventory inventory = player.getInventory();
         ShopMain shopMain = core.getShop();
-
-        if (arg.equalsIgnoreCase("help")) {
-            System.out.println("Shop help 1/3");
-            System.out.println("/shop foods");
-            System.out.println("/shop toys");
-            System.out.println(" ");
-            System.out.println("/shop buy [item]");
-            System.out.println("/shop sell [item]");
-            return;
-        }
-
-        if (arg.equalsIgnoreCase("toys")){
-            System.out.println("List of toys");
-            for (ToyType toyType : shopMain.getToys()){
-                System.out.println(toyType + " - " + toyType.getValue() +   " - "  + toyType.getCost() + "$");
-            }
-            return;
-        }
-
-        if (arg.equalsIgnoreCase("foods")){
-            System.out.println("List of foods:");
-            for (FoodType foodType : shopMain.getFoods()){
-                System.out.println(foodType.getName() + " - " + foodType.getValue() +  " - " + foodType.getCost() + "$");
-            }
-            return;
-        }
-
-
         ShopAction shopAction = shopMain.getActions();
         List<DefaultType> defaultTypes = shopMain.getAllItems();
+        switch (arg) {
+            case "help":
+                System.out.println("Shop help 1/3");
+                System.out.println("/shop foods");
+                System.out.println("/shop toys");
+                System.out.println("/shop baths");
+                System.out.println(" ");
+                System.out.println("/shop buy [item]");
+                System.out.println("/shop sell [item]");
+                break;
+            case "toys":
+                System.out.println("List of toys");
+                for (ToyType toyType : shopMain.getToys()){
+                    System.out.println(toyType + " - " + toyType.getValue() +   " - "  + toyType.getCost() + "$");
+                }
+            case "foods":
+                System.out.println("List of foods:");
+                for (FoodType foodType : shopMain.getFoods()){
+                    System.out.println(foodType.getName() + " - " + foodType.getValue() +  " - " + foodType.getCost() + "$");
+                }
+                break;
+            case "buy":
+                if (type == null){
+                    System.out.println("Usage - /shop buy [foods/toys/baths]");
+                    break;
+                }
 
-        if (arg.equalsIgnoreCase("buy")){
+                switch (type) {
+                    case "foods":
+                        if (item == null){
+                            System.out.println("Usage - /shop buy foods [item]");
+                            break;
+                        }
 
-            if (type == null){
-                System.out.println("Usage - /shop buy [foods/toys/baths]");
-                return;
-            }
+                        try{
+                            FoodType.valueOf(item.toUpperCase());
+                        }catch (IllegalArgumentException illegalArgumentException){
+                            System.out.println("Unknown item");
+                            break;
+                        }
 
+                        shopAction.buyItem(core.getPlayer(), ItemType.FOOD, FoodType.valueOf(item.toUpperCase()));
+                        break;
+                    case "toys":
+                        if (item == null){
+                            System.out.println("Usage - /shop buy toys [item]");
+                            break;
+                        }
 
-            if (type.equalsIgnoreCase("foods")){
-
+                        try{
+                            ToyType.valueOf(item.toUpperCase());
+                        }catch (IllegalArgumentException illegalArgumentException){
+                            System.out.println("Unknown item");
+                            break;
+                        }
+                        shopAction.buyItem(core.getPlayer(), ItemType.TOY, ToyType.valueOf(item.toUpperCase()));
+                        break;
+                }
+                System.out.println("Unknown type");
+                break;
+            case "sell":
                 if (item == null){
-                    System.out.println("Usage - /shop buy foods [item]");
-                    return;
+                    System.out.println("Usage - /shop sell [item]");
+                    break;
                 }
 
-                try{
-                    FoodType.valueOf(item.toUpperCase());
-                }catch (IllegalArgumentException illegalArgumentException){
-                    System.out.println("Unknown item");
-                    return;
-                }
-
-                shopAction.buyItem(core.getPlayer(), ItemType.FOOD, FoodType.valueOf(item.toUpperCase()));
-                return;
-            }
-
-            if (type.equalsIgnoreCase("toys")){
-                if (item == null){
-                    System.out.println("Usage - /shop buy toys [item]");
-                    return;
-                }
-
-                try{
-                    ToyType.valueOf(item.toUpperCase());
-                }catch (IllegalArgumentException illegalArgumentException){
-                    System.out.println("Unknown item");
-                    return;
-                }
-
-                shopAction.buyItem(core.getPlayer(), ItemType.TOY, ToyType.valueOf(item.toUpperCase()));
-                return;
-            }
-
-            System.out.println("Unknown type");
-            return;
-        }
-
-        if (arg.equalsIgnoreCase("sell")){
-            if (item == null){
-                System.out.println("Usage - /shop sell [item]");
-                return;
-            }
-
-            for (DefaultType defaultType : defaultTypes){
-                if (!defaultType.getName().equalsIgnoreCase(item)) {
-                    continue;
-                }
-
-
-                for (int id = 0; id < inventory.getSize(); id++) {
-                    if (!inventory.getItem(id).getDefaultType().getName().equals(item)){
+                for (DefaultType defaultType : defaultTypes){
+                    if (!defaultType.getName().equalsIgnoreCase(item)) {
                         continue;
                     }
 
-                    shopAction.sellItem(player, id);
-                    return;
+                    for (int id = 0; id < inventory.getSize(); id++) {
+                        if (!inventory.getItem(id).getDefaultType().getName().equals(item)){
+                            continue;
+                        }
+
+                        shopAction.sellItem(player, id);
+                        break;
+                    }
+
+                    System.out.println("Error - You don't have the item in you inventory");
+                    break;
                 }
-
-                System.out.println("Error - You don't have the item in you inventory");
-                return;
-            }
-
-            System.out.println("Error - Unknown item.");
-            return;
+                System.out.println("Error - Unknown item.");
+                break;
         }
-
         System.out.println("Unknown argument");
-
     }
 }
