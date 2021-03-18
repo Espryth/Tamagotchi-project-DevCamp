8aa5d013677ae8950c64913e1e2c05fa014310ce
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-18 02:29:52 -0600

    Added all the different type of foods

--- Core/src/main/java/com/maimai/tamagotchi/item/food/FoodType.java
+++ Core/src/main/java/com/maimai/tamagotchi/item/food/FoodType.java
@@ -3,12 +3,15 @@ package com.maimai.tamagotchi.item.food;
 import com.maimai.tamagotchi.item.DefaultType;
 
 public enum FoodType implements DefaultType {
-    APPLE("Apple", 10, 5),
-    CARROT("Carrot", 15, 10),
     SEED("Seed", 15, 10),
-    COOKIE("Cookie", 40, 20),
+    APPLE("Apple", 10, 5),
+    BERRIE("Berrie", 15, 10),
+    MANGO("Mango", 30, 15),
+    CARROT("Carrot", 15, 10),
+    LETTUCE("Lettuce", 50, 25),
     FISH("Fish", 30, 15),
-    BEEF("Beef", 50, 25);
+    BEEF("Beef", 50, 25),
+    CHICKEN("Chicken", 15, 10);
 
     private final String name;
     private final double value;
