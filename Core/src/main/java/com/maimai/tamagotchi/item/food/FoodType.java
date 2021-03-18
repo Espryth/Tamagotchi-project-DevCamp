527929fdf7d4538d5564a1a2d246808874ce3380
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-18 07:43:31 -0600

    Aesthetic fix

--- Core/src/main/java/com/maimai/tamagotchi/item/food/FoodType.java
+++ Core/src/main/java/com/maimai/tamagotchi/item/food/FoodType.java
@@ -3,15 +3,15 @@ package com.maimai.tamagotchi.item.food;
 import com.maimai.tamagotchi.item.DefaultType;
 
 public enum FoodType implements DefaultType {
-    SEED("Seed", 15, 10), //HAMSTER,PARROT
-    APPLE("Apple", 10, 5), //HAMSTER,PARROT
-    BERRIE("Berrie", 15, 10), //HAMSTER,PARROT
-    MANGO("Mango", 30, 15), //HAMSTER,PARROT
-    CARROT("Carrot", 15, 10), //HAMSTER,PARROT,RABBIT
-    LETTUCE("Lettuce", 50, 25), //HAMSTER,PARROT,RABBIT
-    FISH("Fish", 30, 15), //CAT,DOG
-    BEEF("Beef", 50, 25), //CAT,DOG
-    CHICKEN("Chicken", 15, 10); //CAT,DOG
+    SEED("Seed", 15, 10), //Hamster,parrot
+    APPLE("Apple", 10, 5), //Hamster,parrot
+    BERRIE("Berrie", 15, 10), //Hamster,parrot
+    MANGO("Mango", 30, 15), //Hamster,parrot
+    CARROT("Carrot", 15, 10), //Hamster,parrot,rabbit
+    LETTUCE("Lettuce", 50, 25), //Hamster,parrot,rabbit
+    FISH("Fish", 30, 15), //Cat,dog
+    BEEF("Beef", 50, 25), //Cat,dog
+    CHICKEN("Chicken", 15, 10); //Cat,dog
 
     private final String name;
     private final double value;
