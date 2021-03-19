31c0b0348e73db4e655e77d7f6f87c0beae6ad46
Mopo7u7 <45182601+iMopo7u7@users.noreply.github.com>
2021-03-19 16:07:50 -0600

    Fixes

--- API/src/main/java/com/maimai/tamagotchi/Model.java
+++ API/src/main/java/com/maimai/tamagotchi/Model.java
@@ -1,7 +1,5 @@
 package com.maimai.tamagotchi;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public interface Model {
 
     String getId();
--- API/src/main/java/com/maimai/tamagotchi/entity/Entity.java
+++ API/src/main/java/com/maimai/tamagotchi/entity/Entity.java
@@ -1,8 +1,5 @@
 package com.maimai.tamagotchi.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.maimai.tamagotchi.Model;
-
 public interface Entity {
 
     String getName();
